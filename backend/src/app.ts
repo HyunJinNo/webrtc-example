db1feb0e3d95d5718d6c2e2c2b214acb46ff0eea
HyunJinNo <nhj9831@naver.com>
2025-11-18 17:23:19 +0900

    fix: 서버 설정 오류 수정

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 
 const app = express();
-const server = createServer();
+const server = createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "*",
--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 // socket.io 인스턴스 생성
-const socket = io("http://localhost:4000");
+const socket = io("http://192.168.45.126:4000");
 
 const localVideo = document.getElementById("localVideo");
 const remoteVideo = document.getElementById("remoteVideo");
